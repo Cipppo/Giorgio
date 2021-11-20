ce72b61c1ef0d1919c53d5ceb5655ada9f4bdbd0
Luca Rapolla <luca.rapolla@studio.unibo.it>
2021-11-21 00:52:12 +0100

    Added SetStatus

--- src/cippo/Giorgio/Giorgio.java
+++ src/cippo/Giorgio/Giorgio.java
@@ -3,14 +3,18 @@ package cippo.Giorgio;
 import javax.security.auth.login.LoginException;
 
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDA.Status;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
 
 public class Giorgio {
     
     private final JDA jda;
 
     public Giorgio(String token) throws LoginException {
-        this.jda = JDABuilder.createDefault(token).build();
+        this.jda = JDABuilder.createDefault(token).setStatus(OnlineStatus.ONLINE).build();
     }
+    
+
     
 }
