b9fb6c1b19253b1df7741fa5b02295b1ab2c4f8c
Luca Rapolla <luca.rapolla@studio.unibo.it>
2021-11-21 20:04:22 +0100

    Add awaitReady()

--- src/cippo/Giorgio/GiorgioImpl.java
+++ src/cippo/Giorgio/GiorgioImpl.java
@@ -4,7 +4,7 @@ import javax.security.auth.login.LoginException;
 
 public class GiorgioImpl {
     
-    public static void main(String[] arg) throws LoginException {
+    public static void main(String[] arg) {
         
         Giorgio giorgio = new Giorgio("");
         
