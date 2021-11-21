9ca30aad5c0c3dad4822e1ddba5fb5beaa7ddc95
Luca Rapolla <luca.rapolla@studio.unibo.it>
2021-11-21 20:04:03 +0100

    Add awaitReady()

--- src/cippo/Giorgio/Giorgio.java
+++ src/cippo/Giorgio/Giorgio.java
@@ -1,20 +1,24 @@
 package cippo.Giorgio;
 
+import java.util.EventListener;
+
 import javax.security.auth.login.LoginException;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDA.Status;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.TextChannel;
 
 public class Giorgio {
     
     private final JDA jda;
 
-    public Giorgio(String token) throws LoginException {
-        this.jda = JDABuilder.createDefault(token).setStatus(OnlineStatus.ONLINE).build();
+    public Giorgio(String token) throws LoginException, InterruptedException {
+        this.jda = JDABuilder.createDefault(token).setStatus(OnlineStatus.ONLINE).build().awaitReady();
     }
     
-
+    
+  
     
 }
