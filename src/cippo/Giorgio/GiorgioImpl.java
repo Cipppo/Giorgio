fe0c8ab9361a9f2006677204375973051f77dc4a
Luca Rapolla <luca.rapolla@studio.unibo.it>
2021-11-21 21:48:04 +0100

    Removed Unused Import

--- src/cippo/Giorgio/GiorgioImpl.java
+++ src/cippo/Giorgio/GiorgioImpl.java
@@ -6,14 +6,27 @@ import javax.security.auth.login.LoginException;
 
 public class GiorgioImpl {
     
+    
+    
+    
+    
+    
     public static void main(String[] arg) {
         
+        Giorgio giorgio = null;
+        
+        
         final keyReader reader = new keyReader("Key.txt");
+        
         try {
-            final Giorgio giorgio = new Giorgio(reader.read());
+            giorgio = new Giorgio(reader.read());
         } catch (LoginException | InterruptedException | IOException e) {
             e.printStackTrace();
         }
+        
+        
+        giorgio.SpawnMessage();
+        
         
     }
     
--- src/cippo/Giorgio/ReadyListener.java
+++ src/cippo/Giorgio/ReadyListener.java
@@ -3,15 +3,12 @@ package cippo.Giorgio;
 import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.ReadyEvent;
 
-public class ReadyListener implements net.dv8tion.jda.api.hooks.EventListener{
+public class ReadyListener implements net.dv8tion.jda.api.hooks.EventListener {
 
     @Override
     public void onEvent(GenericEvent event) {
         if(event instanceof ReadyEvent) {
             System.out.println("Bot Ready !");
         }
-        
     }
-    
-    
 }
