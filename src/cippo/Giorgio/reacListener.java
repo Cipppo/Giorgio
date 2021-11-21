3a86d881bab78f03b2ccb2f4bf581aea5699975b
Luca Rapolla <luca.rapolla@studio.unibo.it>
2021-11-21 21:45:36 +0100

    Implemented ReactionListener

---
+++ src/cippo/Giorgio/reacListener.java
@@ -0,0 +1,12 @@
+package cippo.Giorgio;
+
+import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class reacListener extends ListenerAdapter{
+    
+    public void onMessageReactionAdd(MessageReactionAddEvent event) {
+        
+        System.out.println("Reaction Added");
+    }
+}
