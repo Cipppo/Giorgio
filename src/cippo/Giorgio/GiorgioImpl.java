0ef36be42e6e2eb5307d21c1554363719bc09205
Luca Rapolla <luca.rapolla@studio.unibo.it>
2021-11-21 20:32:53 +0100

    Implement keyreader

--- src/cippo/Giorgio/GiorgioImpl.java
+++ src/cippo/Giorgio/GiorgioImpl.java
@@ -1,12 +1,19 @@
 package cippo.Giorgio;
 
+import java.io.IOException;
+
 import javax.security.auth.login.LoginException;
 
 public class GiorgioImpl {
     
     public static void main(String[] arg) {
         
-        final Giorgio giorgio = new Giorgio("");
+        final keyReader reader = new keyReader("Key.txt");
+        try {
+            final Giorgio giorgio = new Giorgio(reader.read());
+        } catch (LoginException | InterruptedException | IOException e) {
+            e.printStackTrace();
+        }
         
     }
     
