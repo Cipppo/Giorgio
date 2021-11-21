0b6e63456d252dd4f6eac950a6355e80a4dce3ff
Luca Rapolla <luca.rapolla@studio.unibo.it>
2021-11-21 01:22:13 +0100

    removed testGiorgio and added GiorgioImpl

---
+++ src/cippo/Giorgio/GiorgioImpl.java
@@ -0,0 +1,13 @@
+package cippo.Giorgio;
+
+import javax.security.auth.login.LoginException;
+
+public class GiorgioImpl {
+    
+    public static void main(String[] arg) throws LoginException {
+        
+        Giorgio giorgio = new Giorgio("OTExNjcxMDA3NTU3ODQ5MjQw.YZkxyg.IuOuLRxf-CSOTb6rxS4doAfNcfU");
+        
+    }
+    
+}
