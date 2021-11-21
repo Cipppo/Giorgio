ab935b6bab7184461e36281beedb365fb1cab518
Luca Rapolla <luca.rapolla@studio.unibo.it>
2021-11-21 20:43:42 +0100

    Add ReadyEvent Eventlistener

--- src/cippo/Giorgio/Giorgio.java
+++ src/cippo/Giorgio/Giorgio.java
@@ -9,16 +9,18 @@ import net.dv8tion.jda.api.JDA.Status;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.GenericEvent;
+import net.dv8tion.jda.api.events.ReadyEvent;
 
 public class Giorgio {
     
     private final JDA jda;
 
     public Giorgio(String token) throws LoginException, InterruptedException {
-        this.jda = JDABuilder.createDefault(token).setStatus(OnlineStatus.ONLINE).build().awaitReady();
+        this.jda = JDABuilder.createDefault(token).setStatus(OnlineStatus.ONLINE).addEventListeners(new cippo.Giorgio.ReadyEvent()).build().awaitReady();
     }
     
-    
+
   
     
 }
